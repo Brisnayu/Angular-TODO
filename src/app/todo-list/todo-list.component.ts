e46af0f6fbd9c278730238202cf55e34c62ace8c
Brisna A. Paez M <124035708+Brisnayu@users.noreply.github.com>
2024-03-05 11:31:20 +0100

    Updated information in localStorage

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TodoComponent } from '../shared/components/todo/todo.component';
 import { Todo } from '../models/todo.models';
 import { CreateTodoComponent } from '../shared/components/create-todo/create-todo.component';
+
+const TODO_LIST_KEY = 'TODO_LIST';
 
 @Component({
   selector: 'app-todo-list',
@@ -11,10 +13,22 @@ import { CreateTodoComponent } from '../shared/components/create-todo/create-tod
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.scss',
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
   public todos: Todo[] = [];
+
+  public ngOnInit() {
+    const todoList = window.localStorage.getItem(TODO_LIST_KEY);
+    if (todoList) {
+      this.todos = JSON.parse(todoList);
+    }
+  }
 
   public addTodo(todo: Todo) {
     this.todos.push(todo);
+    this.persistTodos();
+  }
+
+  public persistTodos() {
+    window.localStorage.setItem(TODO_LIST_KEY, JSON.stringify(this.todos));
   }
 }
